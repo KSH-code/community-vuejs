5cde6e1632507aea0bd20155e34d922e718c7e18
Kim SeongHoon <tjdgnsqn133@gmail.com>
2017-07-19 10:41:50 +0900

    vuejs를 사용해봤다.
    webpack을 깔아야 돼니, 집에서 해봐야함..

---
+++ community-vuejs-master/README.md
@@ -0,0 +1,2 @@
+# community-vuejs
+Vuejs 사용자들의 커뮤니티
---
+++ community-vuejs-master/vue/header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav class="h_nav">
+            <a href="./index.html"><img src="img/logo.png" alt=""></a>
+            <ul>
+                <li v-for="list in lists">
+                    <a href="#">{{list}}</a>
+                </li>
+            </ul>
+            <a href="/login.html"></a>
+        </nav>
+    </header>
+</template>
+<script>
+    module.exports ={
+        data: ()=>{
+            return {
+                lists: ['Questions', 'Know-how', 'Meet']
+            }
+        }
+    }
+</script>
